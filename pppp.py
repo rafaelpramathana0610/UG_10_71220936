fff0d649fb98d8dbcdec233ec0b77c0fcb367658
rafaelpramathana0610 <117884225+rafaelpramathana0610@users.noreply.github.com>
2022-11-24 13:19:23 +0700

    Add files via upload

---
+++ pppp.py
@@ -0,0 +1,14 @@
+nama = input("Siapa nama kamu: ")
+umur = input("Berapa umur kamu: ")
+# menampilkan output
+print ("Hello",nama,"umur kamu adalah",umur,"tahun")
+
+nama = input("Nama: ")
+print ("Hello {} apa kabar?".format(nama))
+
+a = 5
+print(a, "bertipe ", type(a))
+a = 2.0
+print(a, "bertipe ", type(a))
+a = 1+2j
+print(a, "Bertipe ", type(a))
